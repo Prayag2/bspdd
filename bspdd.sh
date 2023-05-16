74f57357eaef3bb12d3280822cf09024176db698
Prayag Jain <prayagjain2@gmail.com>
2023-05-16 18:30:58 +0530

    initial support for multi-monitor setups

--- bspdd.sh
+++ bspdd.sh
@@ -1,19 +1,37 @@
 #!/usr/bin/env bash
 
-# VARIABLES
+# Variables
 new_desktop_name=""
+current_monitor="focused"
+help_text="Usage: bspdd.sh [options]\
+    \nOptions: \
+    \n\t-n <name of new desktop>\
+    \n\t-m <name of the selected monitor (uses focused monitor by default)>\n"
+
+# Parse Arguments
+while getopts "n:m:h" opt; do
+    case $opt in
+        n)
+            new_desktop_name=$OPTARG ;;
+        m)
+            current_monitor=$OPTARG ;;
+        h|\?)
+            printf "$help_text"
+            exit 1 ;;
+    esac
+done
 
 # FUNCTIONS
 get_total() {
-    echo $(bspc query -D | wc -l)
+    echo $(bspc query -D -m "$current_monitor" | wc -l)
 }
 get_occupied() {
-    echo $(bspc query -D -d ".occupied" | wc -l)
+    echo $(bspc query -D -d ".occupied" -m "$current_monitor" | wc -l)
 }
 
 add_desktop() {
     echo "Adding desktop"
-    bspc monitor -a "$new_desktop_name"
+    bspc monitor "$current_monitor" -a "$new_desktop_name"
 }
 
 remove_desktops() {
@@ -28,7 +46,7 @@ remove_desktops() {
     done
 }
 
-make_desktops(){
+manage_desktops(){
     total_desktops=$(get_total)
     occupied_desktops=$(get_occupied)
 
@@ -39,9 +57,9 @@ make_desktops(){
     fi
 }
 
-make_desktops
+manage_desktops
 bspc subscribe node_add node_remove node_transfer | while read -r line;
 do
     printf "Total: $total_desktops\nOccupied: $occupied_desktops\n"
-    make_desktops
+    manage_desktops
 done
